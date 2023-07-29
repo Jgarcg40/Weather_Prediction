56de234a70cd56a7b224c5781edea1478358c382
Jonatan Garcia Gonzalez <Jgarcg40@estudiantes.unileon.es>
2023-07-29 04:24:40 +0200

    cambio main.py para que ejecute todo desde ahí-

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ def main():
     try:
         subprocess.run(['python', 'AEMET_API_Historico-Leon.py'], check=True)
         print("AEMET_API_Historico-Leon.py se ha ejecutado correctamente")
+        print("Ejecutando Train_Model.py desde el main...")
+        subprocess.run(['python', 'Train_Model.py'], check=True)
+        print("Train_Model.py se ha ejecutado correctamente")
     except subprocess.CalledProcessError as e:
-        print(f"Error al ejecutar AEMET_API_Historico-Leon.py {e.returncode}")
+        print(f"Error al ejecutar un archivo: {e.returncode}")
 
 if __name__ == "__main__":
     main()
